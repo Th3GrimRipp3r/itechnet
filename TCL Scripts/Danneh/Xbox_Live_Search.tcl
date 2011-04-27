87fac1085de7694f6a09a7cd482a0facd8219091
Dan <Dan@GeeksIRC.com>
2011-04-27 02:03:23 +0000

    Sorted out the stupid spacing.. Or it better have done >:(

--- TCL Scripts/Danneh/Xbox_Live_Search.tcl
+++ TCL Scripts/Danneh/Xbox_Live_Search.tcl
@@ -13,38 +13,39 @@ set xblchar "!"
 proc xblinfo {nick host hand chan search} {
   if {[lsearch -exact [channel info $chan] +xblinfo] != -1} {
     set xbllogo "\0039X\00314box Live\003"
-	set xblsite "Live.xbox.com"
-	if {$search == ""} {
-	  putserv "PRIVMSG $chan :$xbllogo, Please enter a GamerTag to search for.."
-	} else {
-	  set xblsrcurl1 [urlencode [regsub -all { } $search +]]
-	  set xblsrcurl2 "/en-GB/member/${xblsrcurl1}"
-	  if {[catch {set xblsrcsock [socket -async $xblsite 80]} sockerr]} {
-	    putlog "$xbosite $xbosrcurl2 $sockerr error"
-		return 0
+    set xblsite "Live.xbox.com"
+    if {$search == ""} {
+      putserv "PRIVMSG $chan :$xbllogo, Please enter a GamerTag to search for.."
+    } else {
+      set xblsrcurl1 [urlencode [regsub -all { } $search +]]
+      set xblsrcurl2 "/en-GB/member/${xblsrcurl1}"
+      if {[catch {set xblsrcsock [socket -async $xblsite 80]} sockerr]} {
+        putlog "$xbosite $xbosrcurl2 $sockerr error"
+        return 0
       } else {
-	    puts $xblsrcsock "GET $xblsrcurl2 HTTP/1.0"
-		puts $xblsrcsock "Host: $xblsite"
-		puts $xblsrcsock "User-Agent: Opera 9.6"
-		puts $xblsrcsock ""
-		flush $xblsrcsock
-		while {![eof $xblsrcsock]} {
-		  set xblstatus " [gets $xblsrcsock] "
-		  if {[regexp {<div id="CurrentActivity">(.*)<\/div>} $xblstatus match xbluserstatus]} {
-		    putserv "PRIVMSG $chan :$xbllogo [recode "${xbluserstatus}"]"
-	      }
-		}
+        puts $xblsrcsock "GET $xblsrcurl2 HTTP/1.0"
+        puts $xblsrcsock "Host: $xblsite"
+        puts $xblsrcsock "User-Agent: Opera 9.6"
+        puts $xblsrcsock ""
+        flush $xblsrcsock
+        while {![eof $xblsrcsock]} {
+          putlog $xblsrcsock
+          set xblstatus " [gets $xblsrcsock] "
+          if {[regexp {<div id="CurrentActivity">(.*)<\/div>} $xblstatus match xbluserstatus]} {
+            putserv "PRIVMSG $chan :$xbllogo [recode "${xbluserstatus}"]"
+          }
+        }
       }
-	}
+    }
   }
 }
-	  
+          
 proc urlencode {string} {
   return [subst [regsub -nocase -all {([^a-z0-9+])} $string {%[format %x [scan "\\&" %c]]}]]
 }
 proc recode { textin } {
   return [string map {&quot; \" &middot; · &amp; &} [subst [regsub -nocase -all {&#([0-9]{1,5});} $textin {\u\1}]]]
 }
-bind pub -|- ${xblchar}xbl xblinfo
+bind pub - ${xblchar}xbl xblinfo
 setudef flag xblinfo
 putlog "Xbox Live Gamertag status' by Danneh/GrimReaper"
