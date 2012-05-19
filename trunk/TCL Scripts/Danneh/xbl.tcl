361788daa0b86807df3a949809a63d7efe3acccc
DanGeeksIRC@gmail.com <DanGeeksIRC@gmail.com@1d0f1f5d-15f5-2312-d939-fa87e2cc081f>
2012-05-19 16:49:26 +0000

    Updated the code in here to my version with GamerScore.. not that it will make any difference, As I won't be updating it anymore. lol

--- trunk/TCL Scripts/Danneh/xbl.tcl
+++ trunk/TCL Scripts/Danneh/xbl.tcl
@@ -20,7 +20,7 @@ proc xblinfo {nick host hand chan search} {
     } else {
       set xblsrcurl1 [urlencode [regsub -all { } $search +]]
       set xblsrcurl2 [encurl $search]
-      set xblsrcurl3 "/en-GB/member/${xblsrcurl2}"
+      set xblsrcurl3 "/en-GB/Profile?gamertag=${xblsrcurl2}"
       if {[catch {set xblsrcsock [socket -async $xblsite 80]} sockerr]} {
         return 0
       } else {
@@ -32,11 +32,11 @@ proc xblinfo {nick host hand chan search} {
         while {![eof $xblsrcsock]} {
           set xblstatus "[gets $xblsrcsock]"
           if {$xblstatusfound == "on"} {
-            putserv "PRIVMSG $chan :$xbllogo status for \0037$search\003: [string trimleft [recode "${xblstatus}"]]"
+            putserv "PRIVMSG $chan :$xbllogo status for \0037$search\003: [xblstrip [string trimleft [recode "${xblstatus}"]]]"
             close $xblsrcsock
             return 0
           }
-          if {[regexp -all {<div id="CurrentActivity">} $xblstatus]} {
+          if {[regexp -all {<div class="custom">} $xblstatus]} {
             set xblstatusfound "on"
           }
         }
@@ -48,16 +48,54 @@ proc xblinfo {nick host hand chan search} {
   }
 }
  
+proc xblgsinfo {nick host hand chan search} {
+  if {[lsearch -exact [channel info $chan] +xblinfo] != -1} {
+    set xbllogo "\0039X\00314box Live\003"
+    set xblsite "Live.xbox.com"
+    if {$search == ""} {
+      putserv "PRIVMSG $chan :$xbllogo, Please enter a GamerTag to search GamerScore for.."
+    } else {
+      set xblsrcurl1 [urlencode [regsub -all { } $search +]]
+      set xblsrcurl2 [encurl $search]
+      set xblsrcurl3 "/en-GB/Profile?gamertag=${xblsrcurl2}"
+      if {[catch {set xblsrcsock [socket -async $xblsite 80]} sockerr]} {
+        return 0
+      } else {
+        puts $xblsrcsock "GET $xblsrcurl3 HTTP/1.0"
+        puts $xblsrcsock "Host: $xblsite"
+        puts $xblsrcsock "User-Agent: Opera 9.6"
+        puts $xblsrcsock ""
+        flush $xblsrcsock
+        while {![eof $xblsrcsock]} {
+          set xblstatus "[gets $xblsrcsock]"
+          if {[regexp -all {<div class="gamerscore">(.*?)</div>} $xblstatus match xblgamerscore]} {
+            putserv "PRIVMSG $chan :$xbllogo GamerScore for \0037$search\003: $xblgamerscore"
+	     close $xblsrcsock
+	     return 0
+          }
+        }
+	 putserv "PRIVMSG $chan :Sorry, I couldn't find \0037$search\003 on $xbllogo"
+        close $xblsrcsock
+        return 0
+      }
+    }
+  }
+}
+ 
+ 
 proc encurl {string} {
   return [string map {" " %20} "$string"]
 }
- 
+proc xblstrip {string} {
+  return [regsub -all {<[^<>]+>} $string ""]
+} 
 proc urlencode {string} {
   return [subst [regsub -nocase -all {([^a-z0-9+])} $string {%[format %x [scan "\\&" %c]]}]]
 }
 proc recode { textin } {
-  return [string map {&quot; \" &middot; · &amp; &} [subst [regsub -nocase -all {&#([0-9]{1,5});} $textin {\u\1}]]]
+  return [string map {&#174; \174 &quot; \" &middot; · &amp; &} [subst [regsub -nocase -all {&#([0-9]{1,5});} $textin {\u\1}]]]
 }
 bind pub - ${xblchar}xbl xblinfo
+bind pub - ${xblchar}xblgs xblgsinfo
 setudef flag xblinfo
 putlog "Xbox Live Gamertag status' by Danneh/GrimReaper"
